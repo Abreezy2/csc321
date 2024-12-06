62f39daa3100edb0fd230045a48ec3468bde5b6d
ay <ayonnas@csudh.edu>
2024-12-06 03:59:52 +0000

    lab 4

---
+++ ayonnas4.c
@@ -0,0 +1,30 @@
+//Adonay Yonnas
+//csc-321
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (void)
+{
+	int x = 0;
+	double y = 0.0;
+	char c = 'a';
+	float z = 0.0;
+
+	printf("enter an integer: ");
+	scanf("%d",&x);
+
+	if(x > 5)
+	{
+		printf("You input a number larger than 5 \n");
+	}
+	else if(x == 5)
+	{
+		printf("You input 5 \n");
+	}
+	else
+	{
+		printf("You entered a number less than 5 \n");
+	}
+	return EXIT_SUCCESS;
+}
